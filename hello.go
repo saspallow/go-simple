b32b88e8300cdf9756c48a4110bb39dc52730e72
Surasak Potipipit <SPotipipit@dstworldwideservices.com>
2017-08-18 12:03:56 +0700

    Mux

--- hello.go
+++ hello.go
@@ -7,30 +7,25 @@ import (
 
 func main() {
     // h := indexHandler{}
-    h := http.HandlerFunc(mux)
-    err := http.ListenAndServe(":8900", h)
+    // h := http.HandlerFunc(mux)
+    // err := http.ListenAndServe(":8900", h)
+    // log.Println(err)
+
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", indexHandler)
+    mux.HandleFunc("/about", aboutHandler)
+    err := http.ListenAndServe(":8900", mux)
     log.Println(err)
 }
 
-func mux(w http.ResponseWriter, r *http.Request) {
-    switch r.URL.Path {
-        case "/":
-            indexHandler(w, r)
-        case "/about":
-            aboutHandler(w, r)
-        default:
-            notFoundHandler(w, r)
-    }
-}
-
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
     w.Write([]byte("Index Page"))
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("About Page"))
-}
-
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("404 Page Not Found"))
 }
