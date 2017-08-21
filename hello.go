1ee219564051d20c3b183e33f2a65ace2571b86c
Surasak Potipipit <SPotipipit@dstworldwideservices.com>
2017-08-21 17:15:43 +0700

    Channel

--- hello.go
+++ hello.go
@@ -1,63 +1,19 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"fmt"
 )
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.Handle("/admin", allowRoles("admin")(http.HandlerFunc(adminHandler)))
-	http.Handle("/staff", allowRoles("staff")(http.HandlerFunc(staffHandler)))
-	http.Handle("/admin-staff", allowRoles("admin", "staff")(http.HandlerFunc(adminStaffHandler)))
-	err := http.ListenAndServe(":8900", nil)
-	log.Println(err)
+	workChan := make(chan string)
+	go worker(workChan)
+	workChan <- "Saspallow"
+	workChan <- "Hacker"
 }
 
-type middleware func(http.Handler) http.Handler
-
-func allowRole(role string) middleware {
-	return func(h http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			reqRole := r.Header.Get("Role")
-			if reqRole != role {
-				http.Error(w, "Forbidden", http.StatusForbidden)
-				return
-			}
-			h.ServeHTTP(w, r)
-		})
+func worker(ch chan string) {
+	for {
+		name := <-ch
+		fmt.Println("Hello, ", name)
 	}
-}
-
-func allowRoles(roles ...string) middleware {
-	allow := make(map[string]struct{})
-	for _, role := range roles {
-		allow[role] = struct{}{}
-	}
-	return func(h http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			reqRole := r.Header.Get("Role")
-			if _, ok := allow[reqRole]; !ok {
-				http.Error(w, "Forbidden", http.StatusForbidden)
-				return
-			}
-			h.ServeHTTP(w, r)
-		})
-	}
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("\nHello"))
-}
-
-func adminHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("\nHello, Admin."))
-}
-
-func staffHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("\nHello, Staff."))
-}
-
-func adminStaffHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, Admin & Staff."))
 }
