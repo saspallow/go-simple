ee6678061e8aaf333f7ebcaad0d5a06c8fdeebb1
Surasak Potipipit <SPotipipit@dstworldwideservices.com>
2017-08-21 17:03:36 +0700

    Allow Role

--- hello.go
+++ hello.go
@@ -6,47 +6,58 @@ import (
 )
 
 func main() {
-	h := chain(
-		m1,
-		m2,
-		m3,
-	)(http.HandlerFunc(indexHandler))
-	err := http.ListenAndServe(":8900", h)
+	http.HandleFunc("/", indexHandler)
+	http.Handle("/admin", allowRoles("admin")(http.HandlerFunc(adminHandler)))
+	http.Handle("/staff", allowRoles("staff")(http.HandlerFunc(staffHandler)))
+	http.Handle("/admin-staff", allowRoles("admin", "staff")(http.HandlerFunc(adminStaffHandler)))
+	err := http.ListenAndServe(":8900", nil)
 	log.Println(err)
 }
 
 type middleware func(http.Handler) http.Handler
 
-func chain(hs ...middleware) middleware {
+func allowRole(role string) middleware {
 	return func(h http.Handler) http.Handler {
-		for i := len(hs); i > 0; i-- {
-			h = hs[i-1](h)
-		}
-		return h
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			reqRole := r.Header.Get("Role")
+			if reqRole != role {
+				http.Error(w, "Forbidden", http.StatusForbidden)
+				return
+			}
+			h.ServeHTTP(w, r)
+		})
 	}
 }
 
-func m1(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("M1")
-		h.ServeHTTP(w, r)
-	})
-}
-
-func m2(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("M2")
-		h.ServeHTTP(w, r)
-	})
-}
-
-func m3(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("m3")
-		h.ServeHTTP(w, r)
-	})
+func allowRoles(roles ...string) middleware {
+	allow := make(map[string]struct{})
+	for _, role := range roles {
+		allow[role] = struct{}{}
+	}
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			reqRole := r.Header.Get("Role")
+			if _, ok := allow[reqRole]; !ok {
+				http.Error(w, "Forbidden", http.StatusForbidden)
+				return
+			}
+			h.ServeHTTP(w, r)
+		})
+	}
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Index Page"))
+	w.Write([]byte("\nHello"))
+}
+
+func adminHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("\nHello, Admin."))
+}
+
+func staffHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("\nHello, Staff."))
+}
+
+func adminStaffHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, Admin & Staff."))
 }
